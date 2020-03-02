732c853f431d5ec7b213305d3bf18e536b76b4d5
RanfordS <wemustbemental@gmail.com>
2020-03-02 15:11:22 +0000

    Table of contents work

--- MakeDoc.lua
+++ MakeDoc.lua
@@ -6,18 +6,42 @@ make_bib = "biber "..jobname
 make_pdf_null = make_pdf.." > /dev/null"
 make_bib_null = make_bib.." > /dev/null"
 
+path_internal = "RSkylInternal/"
+contents_major_new = path_internal.."MajorContents.new"
+contents_major_old = path_internal.."MajorContents.tex"
+contents_remove_old = "rm "..contents_major_old
+contents_shuffle = "mv "..contents_major_new.." "..contents_major_old
+
 file = io.open ("RSkylToC.tex", "w")
 file:write ("\\UNDEF")
 file:close ()
 
+file = io.open (contents_major_old, "w")
+file:write ("\\UNDEF")
+file:close ()
+
+--[[
+function Iter_file_make (i)
+    file = io.open ("RSkylInternal/Iter.tex", "w")
+    if i == 1 then file:write "\\newcommand{\\RIternoprev}{}" end
+    file:write ("\\newcommand{\\RIterprev}{"..(i-1).."}\n")
+    file:write ("\\newcommand{\\Rparsenext}{"..( i ).."}\n")
+    file:close ()
+end
+--]]
+
 print ("\n\27[1;4;33mDoing Repeat Builds\27[0m\n")
 for i = 1, repeats do
+    --Iter_file_make (i)
     print ("iteration:", i)
     os.execute (make_pdf_null)
     os.execute (make_bib_null)
+    os.execute (contents_remove_old)
+    os.execute (contents_shuffle)
 end
 
 print ("\n\27[1;4;33mFinal Build Log\27[0m\n")
+--Iter_file_make (repeats + 1)
 res = io.popen (make_pdf)
 txt = res:read ("*a")
 res:close ()
